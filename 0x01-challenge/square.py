55957020156f0344da458e8e6971254654ccf5ce
AKINSOLA BABAJIDE AMOS <90686696+avgamoz@users.noreply.github.com>
2023-04-12 11:00:12 +0100

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-"""This module contains the class square"""
+""" Module for square class"""
 
 
-class square():
-    """Definations of the class square"""
+class Square():
+    """ Square class """
+    width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Initializer for square class """
+        """ Instantiation of class """
         for key, value in kwargs.items():
             setattr(self, key, value)
-            break
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def permiter_of_mySquare(self):
-        """ Perimeter of the square """
-        return (self.width * 4)
+    def permiter_of_my_square(self):
+        """ Perimeter of my square """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ String format """
-        return "{}/{}".format(self.width, self.width)
+        """ Printable representation """
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    """ Create a square object """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.permiter_of_mySquare())
+    print(s.permiter_of_my_square())
